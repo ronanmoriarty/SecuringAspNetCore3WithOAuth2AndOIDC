ac574db453c11704b2d212d305129dd95a81aea5
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-08-02 19:56:27 +0100

    Prevent free users from being able to navigate to page by typing url

--- Starter files/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ Starter files/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -201,6 +201,7 @@ namespace ImageGallery.Client.Controllers
             await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
         }
 
+        [Authorize(Roles = "PayingUser")]
         public async Task<IActionResult> OrderFrame()
         {
             var idpClient = _httpClientFactory.CreateClient("IDPClient");
