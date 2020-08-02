13a72736054a085a5ddd4ace891c8ab664d43372
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-08-02 15:33:38 +0100

    Configure gallery client for identity server

--- Starter files/src/My.IDP/Config.cs
+++ Starter files/src/My.IDP/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4;
 
 namespace My.IDP
 {
@@ -11,8 +12,9 @@ namespace My.IDP
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -20,7 +22,27 @@ namespace My.IDP
             { };
 
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+            new[]
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
     }
 }
