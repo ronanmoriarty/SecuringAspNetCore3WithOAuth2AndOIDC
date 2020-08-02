8f56c17e254cd7bccf46643f663a9e79ce033b53
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-08-02 15:34:17 +0100

    Configure gallery client

--- Starter files/src/ImageGallery.Client/Startup.cs
+++ Starter files/src/ImageGallery.Client/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
 using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace ImageGallery.Client
 {
@@ -29,7 +31,26 @@ namespace ImageGallery.Client
                 client.BaseAddress = new Uri("https://localhost:44366/");
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
-            });             
+            });
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                    {
+                        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.Authority = "https://localhost:5001/";
+                        options.ClientId = "imagegalleryclient";
+                        options.ResponseType = "code";
+                        // options.CallbackPath = new PathString("...");
+                        options.Scope.Add("openid");
+                        options.Scope.Add("profile");
+                        options.SaveTokens = true;
+                        options.ClientSecret = "secret";
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +73,9 @@ namespace ImageGallery.Client
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
