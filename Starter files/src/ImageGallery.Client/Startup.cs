a6e94bf712ba11517a987b24f86baeafd51f87fc
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-08-02 17:07:37 +0100

    Remove default claim transforms

--- Starter files/src/ImageGallery.Client/Startup.cs
+++ Starter files/src/ImageGallery.Client/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
@@ -17,6 +18,7 @@ namespace ImageGallery.Client
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
